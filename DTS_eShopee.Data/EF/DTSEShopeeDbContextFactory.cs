7bc82e0d26b39d70bcf26eb9863b40d195378114
SangDoThanh <sangdothanh95@gmail.com>
2025-03-18 21:43:07 +0700

    Migration database

---
+++ DTS_eShopee.Data/EF/DTSEShopeeDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DTS_eShopee.Data.EF
+{
+    public class DTSEShopeeDbContextFactory : IDesignTimeDbContextFactory<DTSEShopeeDbContext>
+    {
+        public DTSEShopeeDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DTS_eShopeeDatabase");
+
+            var optionsBuilder = new DbContextOptionsBuilder<DTSEShopeeDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new DTSEShopeeDbContext(optionsBuilder.Options);
+        }
+    }
+}
