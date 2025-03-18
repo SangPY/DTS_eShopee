dd05bbd83217c7e5bd5828048663616afa0de197
sangdothanh95 <sangdothanh95@gmail.com>
2025-03-18 13:57:03 +0700

    Create Entity classes

---
+++ DTS_eShopee.Data/Entities/Contact.cs
@@ -0,0 +1,23 @@
+﻿using DTS_eShopee.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTS_eShopee.Data.Entities
+{
+    public class Contact
+    {
+        public int Id { set; get; }
+
+        public string Name { set; get; }
+
+        public string Email { set; get; }
+
+        public string PhoneNumber { set; get; }
+
+        public string Message { set; get; }
+
+        public Status Status { set; get; }
+
+    }
+}
---
+++ DTS_eShopee.Data/Enums/Status.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTS_eShopee.Data.Enums
+{
+    public enum Status
+    {
+        InActive,
+        Active
+    }
+}
