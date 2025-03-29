17aec757bb872e653f828a5c6b1d447ab7179f1a
SangDoThanh <sangdothanh95@gmail.com>
2025-03-29 23:38:10 +0700

    Filter user

--- DTS_eShopee.AdminApp/Controllers/UserController.cs
+++ DTS_eShopee.AdminApp/Controllers/UserController.cs
@@ -44,7 +44,7 @@ namespace DTS_eShopee.AdminApp.Controllers
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             HttpContext.Session.Remove("Token");
-            return RedirectToAction("Login", "User");
+            return RedirectToAction("Index", "Login");
         }
 
         [HttpGet]
