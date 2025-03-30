948acadc24d39b5515d362b6a8d61b7b2903eecb
SangDoThanh <sangdothanh95@gmail.com>
2025-03-31 00:32:36 +0700

    View Product list

--- DTS_eShopee.BackendApi/Controllers/ProductsController.cs
+++ DTS_eShopee.BackendApi/Controllers/ProductsController.cs
@@ -29,10 +29,10 @@ namespace DTS_eShopee.BackendApi.Controllers
         //}
 
         //http://localhost:port/product/public-paging
-        [HttpGet("{languageId}")]
-        public async Task<IActionResult> GetAllPaging(string languageId, [FromQuery] GetPublicProductPagingRequest request)
+        [HttpGet("paging")]
+        public async Task<IActionResult> GetAllPaging([FromQuery] GetManageProductPagingRequest request)
         {
-            var products = await _productService.GetAllByCategoryId(languageId, request);
+            var products = await _productService.GetAllPaging(request);
             return Ok(products);
         }
 
